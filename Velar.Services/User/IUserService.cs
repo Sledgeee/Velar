ef2fd079aaaf1b4fade95bd4b3ba918f6b30ee4a
Oleh Voloshyn <76629564+Sledgeee@users.noreply.github.com>
2022-10-21 23:09:00 +0300

    Delete Velar.Services directory

--- Velar.Services/User/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Velar.Services.User
-{
-    public interface IUserService
-    {
-        Infrastructure.Models.UserModel.User FindByEmail(string email);
-    }
-}
